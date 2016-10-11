5855c06a516b4bc66098d904481b266a6f08cd54
haroturbo <imasperfume@gmail.com>
2016-10-11 22:13:52 +0900

    bintrans

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,11 +37,15 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(51, 29);
+            this.textBox1.Location = new System.Drawing.Point(59, 7);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(183, 19);
             this.textBox1.TabIndex = 0;
@@ -49,7 +53,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(50, 54);
+            this.textBox2.Location = new System.Drawing.Point(59, 32);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(183, 19);
             this.textBox2.TabIndex = 1;
@@ -58,7 +62,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 32);
+            this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 4;
@@ -67,7 +71,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 57);
+            this.label2.Location = new System.Drawing.Point(12, 38);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 5;
@@ -76,7 +80,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(4, 95);
+            this.label3.Location = new System.Drawing.Point(12, 64);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 12);
             this.label3.TabIndex = 6;
@@ -84,11 +88,11 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(97, 88);
+            this.textBox3.Location = new System.Drawing.Point(106, 61);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(136, 19);
             this.textBox3.TabIndex = 7;
-            this.textBox3.Text = "zCheat.suprx";
+            this.textBox3.Text = "GoHANmem.suprx";
             // 
             // button4
             // 
@@ -104,36 +108,72 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(4, 119);
+            this.label4.Location = new System.Drawing.Point(12, 86);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(127, 12);
             this.label4.TabIndex = 10;
             this.label4.Text = "パッチ範囲(0x-0xで指定)";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(136, 115);
+            this.textBox4.Location = new System.Drawing.Point(145, 83);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(136, 19);
+            this.textBox4.Size = new System.Drawing.Size(97, 19);
             this.textBox4.TabIndex = 11;
-            this.textBox4.Text = "0xc2a0-0xC6ff";
+            this.textBox4.Text = "0x51430-0x51b50";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 116);
+            this.panel1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "設定ファイル:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "GoHANmem_v1.00",
+            "zCheat_v1.00",
+            "custom"});
+            this.listBox1.Location = new System.Drawing.Point(84, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 16);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 289);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "ばいなりとらんすれーたー";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,6 +190,9 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,17 +15,36 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+            listBox1.Focus();
+            listBox1.SelectedIndex=0;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             try
             {
-            string before_trans = textBox1.Text;
-            string after_trans = textBox2.Text;
+             string basepath = System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, listBox1.Text);
+
+            string before_trans = System.IO.Path.Combine( basepath , textBox1.Text);
+            string after_trans = System.IO.Path.Combine(basepath , textBox2.Text);
             string patch_file = textBox3.Text;
             string range_start = textBox4.Text;
             string[] rang = range_start.Split(Convert.ToChar(0x2d));
+
+
+                if (System.IO.File.Exists(before_trans) == false) { 
+                    MessageBox.Show(before_trans + "翻訳前ファイルが存在しません");
+                    return ;
+                }
+                if (System.IO.File.Exists(after_trans) == false){
+                MessageBox.Show(after_trans + "翻訳後ファイルが存在しません");
+                    return;
+                }
+                if (System.IO.File.Exists(patch_file) ==false){
+
+                    MessageBox.Show(after_trans + "ぱっち対象ファイルが存在しません");
+                    return;
+                }
 
                 if (rang.Length != 2) {
                     MessageBox.Show("範囲を入力して下さい");
@@ -101,6 +120,8 @@ namespace WindowsFormsApplication1
                 }
             }
 
+                string logger = "";
+
             if (t == tt)
             {
                     for (int i = 0; i < t; i++)
@@ -110,7 +131,11 @@ namespace WindowsFormsApplication1
                         {
                             if (bt[i].Length == at[i].Length)
                             {
-                                filestring = Microsoft.VisualBasic.Strings.Replace(filestring,bt[i], at[i], 1, 1, Microsoft.VisualBasic.CompareMethod.Binary);
+                                filestring = Microsoft.VisualBasic.Strings.Replace(filestring, bt[i], at[i], 1, 1, Microsoft.VisualBasic.CompareMethod.Binary);
+                            }
+                            else {
+
+                                logger += (i + 1).ToString() + "行目文字数不一致 前" + (bt[i].Length).ToString() +" 後:"+ (at[i].Length).ToString();
                             }
                         }
                 }
@@ -120,6 +145,7 @@ namespace WindowsFormsApplication1
             else {
 
                 MessageBox.Show("翻訳前てきすとと翻訳後てきすとの行数が一致しません");
+                    return;
             }
             
                 for (int i = 0; i < filestring.Length; i += 2)
@@ -136,12 +162,37 @@ namespace WindowsFormsApplication1
                 ws.Write(bin, 0, bin.Length);
                 ws.Close();
 
+                MessageBox.Show("翻訳パッチが成功しました!!!!");
+
 
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
                 throw;
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string file = System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath ,listBox1.Text,"set.txt");
+            if (System.IO.File.Exists(file))
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(file, System.Text.Encoding.GetEncoding("shift_jis"));
+
+                textBox4.Text = sr.ReadLine();
+                textBox3.Text = sr.ReadLine();
+                sr.Close();
+            }
+            else {
+                MessageBox.Show(file + "設定用が存在しません");
             }
 
         }
